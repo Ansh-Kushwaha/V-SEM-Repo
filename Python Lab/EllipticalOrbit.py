9e96387f8e7f3b9fd2e94b2a2ccdf406b03bbce1
Ansh Kushwaha <ansh.kushwaha26@gmail.com>
2023-08-24 21:55:06 +0530

    Update Python Lab EllipticalOrbit

--- Python Lab/EllipticalOrbit.py
+++ Python Lab/EllipticalOrbit.py
@@ -1,86 +1,78 @@
 import pygame
 import math
 
-au = 1.495978707e8
-G = 6.6743e-11
 width = 1000
 height = 500
+G = 1e-4
 
 pygame.init()
 pygame.display.set_caption("Planet Simulation")
 screen = pygame.display.set_mode([width, height])
 clock = pygame.time.Clock()
 
-class Planet():
-    def __init__(self, name, color, solar_system, mass, pos, vel):
-        self.name = name
-        self.pos = pos
-        self.vel = vel
+class Planet(pygame.sprite.Sprite):
+    def __init__(self, mass, color, pos, vel, rad):
+        super().__init__()
         self.mass = mass
         self.color = color
+        self.pos = pos
+        self.vel = vel
+        self.rad = rad
 
-        solar_system.add_body(self)
+        self.image = pygame.Surface([rad * 2, rad * 2])
+        self.image.fill([0, 0, 0, 0])
+        self.rect = self.image.get_rect()
 
-    def draw(self):
-        pygame.draw.circle(surface=screen, color=self.color, center=self.pos, radius = math.log(self.mass, 4))
-    
-    def update(self, nearby_bodies):
-        self.getVelocity(nearby_bodies)
-        print(self.pos.x, self.pos.y)
-        self.pos.x += self.vel.x
-        self.pos.y += self.vel.y
+        pygame.draw.circle(self.image, self.color, (self.rad, self.rad), self.rad)
 
+    def update(self, planets):
+        for planet in planets:
+            if planet != self:
+                d = pygame.math.Vector2(self.pos).distance_to(planet.pos)
+                if d == 0:
+                    d = 0.000001
+                angle = math.atan2((planet.pos.y - self.pos.y), (planet.pos.x - self.pos.x))
+                force = G * self.mass * planet.mass / d * d
+                ax = (force  * math.cos(angle)) / self.mass
+                ay = (force  * math.sin(angle)) / self.mass
+                # print(angle)
 
-    def getVelocity(self, nearby_bodies):
-        for body in nearby_bodies:
-            if body.name != self.name:
-                dx = body.pos.x - self.pos.x
-                dy = body.pos.y - self.pos.y
-                angle = math.atan2(dy, dx)
+                self.vel.x += ax
+                self.vel.y += ay
 
-                dd = math.sqrt((dx ** 2) + (dy ** 2))
-                if dd == 0:
-                    dd = 0.000001
-                force = self.mass * body.mass / (dd ** 2)
-                print(force)
+                self.pos.x += self.vel.x
+                self.pos.y += self.vel.y
 
-                self.vel.x += (math.cos(angle) * force) / self.mass
-                self.vel.y += (math.sin(angle) * force) / self.mass
-    
+                print(self.pos)
+                # print('Rect', self.rect)
 
+planets = []
 
-class SolarSystem:
-    def __init__(self):
-        self.bodies = []
+sunPos = pygame.Vector2(width // 2 - 60, height // 2 - 60)
+p1Pos = pygame.Vector2(width // 2 + 200, height // 2 - 10)
 
-    def draw(self):
-        screen.fill("black")
-        for body in self.bodies:
-            body.draw()
-            
-    def add_body(self, body):
-        self.bodies.append(body)
+sun = Planet(1000, [253, 184, 19], sunPos, pygame.Vector2(0, 0), 60)
+p1 = Planet(4, [51,194,254], p1Pos, pygame.Vector2(0, 0), 10)
 
-    def remove_body(self, body):
-        self.bodies.remove(body)
+planets.append(sun)
+planets.append(p1)
+
+pygame.Surface.blit(screen, p1.image, p1.pos)
+pygame.Surface.blit(screen, sun.image, sun.pos)
 
 
 def main():
-    s = SolarSystem()
-    Sun = Planet('sun', [253, 184, 19], s, 1.9891e25, pygame.Vector2(width // 2, height // 2), pygame.Vector2(0, 0))
-    P1 = Planet('earth', [0, 0, 160], s, 5.97219e5, pygame.Vector2((width // 2) + 2, height // 2), pygame.Vector2(0, 0))
-    
-    running = True
-    
+    running = True    
     while running:
         clock.tick(1)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        for body in s.bodies:
-            body.update(s.bodies)
-        s.draw()
+        for planet in planets:
+            planet.update(planets)
+            screen.fill([0, 0, 0])
+            pygame.display.update(planet.rect)
         pygame.display.flip()
 
 main()
