4338d56cd4a292ce99ac3a43b4260be15de7cbb6
Ansh Kushwaha <ansh.kushwaha26@gmail.com>
2023-08-24 23:03:45 +0530

    Update Python Lab EllipticalOrbit

--- Python Lab/EllipticalOrbit.py
+++ Python Lab/EllipticalOrbit.py
@@ -3,7 +3,7 @@ import math
 
 width = 1000
 height = 500
-G = 1e-4
+G = 10
 
 pygame.init()
 pygame.display.set_caption("Planet Simulation")
@@ -20,9 +20,9 @@ class Planet(pygame.sprite.Sprite):
         self.rad = rad
 
         self.image = pygame.Surface([rad * 2, rad * 2])
-        self.image.fill([0, 0, 0, 0])
+        self.image.fill([0, 0, 0])
         self.rect = self.image.get_rect()
-
+    
         pygame.draw.circle(self.image, self.color, (self.rad, self.rad), self.rad)
 
     def update(self, planets):
@@ -31,11 +31,14 @@ class Planet(pygame.sprite.Sprite):
                 d = pygame.math.Vector2(self.pos).distance_to(planet.pos)
                 if d == 0:
                     d = 0.000001
-                angle = math.atan2((planet.pos.y - self.pos.y), (planet.pos.x - self.pos.x))
-                force = G * self.mass * planet.mass / d * d
+                dy = self.pos.y - planet.pos.y
+                dx = self.pos.x - planet.pos.x
+                # angle = pygame.math.Vector2(self.pos).angle_to(planet.pos) * math.pi / 180
+                angle = math.atan2(dy, dx)
+                force = self.mass * planet.mass / (d * d)
                 ax = (force  * math.cos(angle)) / self.mass
-                ay = (force  * math.sin(angle)) / self.mass
-                # print(angle)
+                ay = (force  * math.sin(angle)) / self.mass 
+                print(angle)
 
                 self.vel.x += ax
                 self.vel.y += ay
@@ -48,31 +51,36 @@ class Planet(pygame.sprite.Sprite):
 
 planets = []
 
-sunPos = pygame.Vector2(width // 2 - 60, height // 2 - 60)
-p1Pos = pygame.Vector2(width // 2 + 200, height // 2 - 10)
+sunPos = pygame.Vector2(width // 2 - 80, height // 2 - 80)
+p1Pos = pygame.Vector2(width // 2 + 1000, height // 2 - 10)
 
-sun = Planet(1000, [253, 184, 19], sunPos, pygame.Vector2(0, 0), 60)
-p1 = Planet(4, [51,194,254], p1Pos, pygame.Vector2(0, 0), 10)
+sun = Planet(200, [253, 184, 19], sunPos, pygame.Vector2(0, 0), 80)
+p1 = Planet(10, [51,194,254], p1Pos, pygame.Vector2(0, 0), 10)
 
 planets.append(sun)
 planets.append(p1)
 
-pygame.Surface.blit(screen, p1.image, p1.pos)
-pygame.Surface.blit(screen, sun.image, sun.pos)
+group = pygame.sprite.Group()
+group.add(sun)
+group.add(p1)
+
+# pygame.Surface.blit(screen, p1.image, p1.pos)
+# pygame.Surface.blit(screen, sun.image, sun.pos)
 
 
 def main():
     running = True    
     while running:
-        clock.tick(1)
-
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         for planet in planets:
             planet.update(planets)
-            screen.fill([0, 0, 0])
-            pygame.display.update(planet.rect)
+        screen.fill([0, 0, 0])
+        for planet in planets:
+            pygame.Surface.blit(screen, planet.image, planet.pos)
+
         pygame.display.flip()
 
 main()
